404fe740b509ef91086e7e5c425589158a811376
MKMK <4i20xking@gmail.com>
2026-02-03 18:27:51 -0300

    inicio

---
+++ src/main/kotlin/tigre/untitled3/illusion.kt
@@ -0,0 +1,13 @@
+package tigre.illusion
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class IllusionPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        logger.info("alo")
+    }
+
+    override fun onDisable() {
+    }
+}
